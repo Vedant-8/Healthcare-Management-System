4b50c91d83c85f53e5ae3ee6d69b7b17d3a95ca5
Omkar <omkarnova@gmail.com>
2025-01-06 22:13:22 +0530

    added routers

--- server/main.py
+++ server/main.py
@@ -1,19 +1,27 @@
 # main.py
 
 from fastapi import FastAPI
-from routers.map_router import router as map_router
-from server.routers.patient_router import router as appointment_scheduler
 import uvicorn 
 from logging import Logger
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
+import logging
+
+from routers.map_router import router as map_router
+from routers.patient_router import router as patient_router
+from routers.document_router import router as document_router
 
 app = FastAPI()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+ngrok_url="https://e721-103-167-123-72.ngrok-free.app"
 
 #middleware config
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=[ngrok_url],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -22,7 +30,9 @@ app.add_middleware(
 
 # Include the API routes
 app.include_router(map_router, prefix="/api")
-app.include_router(appointment_scheduler, prefix="/api")
+app.include_router(patient_router, prefix="/api")
+app.include_router(document_router, prefix="/api")
+
 
 
 @app.get("/")
@@ -31,4 +41,4 @@ def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
